76fdcf51c9cef1df48223705eb09dec293ec4f15
eu-vinnom <vinnom.2d@gmail.com>
2020-01-30 11:27:05 -0300

    alteração no título da appbar

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PacoteViagensActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PacoteViagensActivity.java
@@ -9,10 +9,13 @@ import android.widget.ListView;
 
 public class PacoteViagensActivity extends AppCompatActivity{
 
+	public static final String TITULO_APPBAR = "Pacotes";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pacote_viagens);
+		setTitle(TITULO_APPBAR);
 
 		ListView listaPacotes = findViewById(R.id.pacote_viagens_lista);
 		listaPacotes.setAdapter(new PacoteViagensAdapter(this));
